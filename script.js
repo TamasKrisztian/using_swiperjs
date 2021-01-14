e2595966c655f452f27e329994dbbc1fd42f5fef
TamasKrisztian <kortanaz@gmail.com>
2021-01-14 09:56:52 +0100

    finished task

---
+++ script.js
@@ -0,0 +1,16 @@
+function pageLoad() {
+
+	let mySwiper = new Swiper('.swiper-container', {
+		loop: true,
+
+		pagination: {
+			el: '.swiper-pagination',
+		},
+
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		}
+	})
+}
+window.addEventListener("load", pageLoad);
